06b43c31b4a1d61ef4c95106ff80b6ffa6197844
loongmxbt <loongmxbt@gmail.com>
2017-07-25 16:29:49 +0800

    deploy

--- deploy.exs
+++ deploy.exs
@@ -5,6 +5,6 @@ defmodule Hdcy.DeployCallbacks do
     # mkdir prevents complains about this directory not existing
     bash("mkdir", ~w[-p priv/static], cd: env.build_dir)
     bash("npm", ~w[install], cd: env.build_dir)
-    bash("npm", ~w[run deploy], env.build_dir)
+    bash("npm", ~w[run deploy], cd: env.build_dir)
   end
 end
